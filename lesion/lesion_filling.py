08808e9be78cc6e777ed86b1cfd27923a8c9a6ca
Ubuntu <ubuntu@ip-172-31-37-230.ec2.internal>
2014-05-29 08:57:26 +0000

    first commit

---
+++ lesion/lesion_filling.py
@@ -0,0 +1,102 @@
+"""
+Arman Eshaghi, May 2014, MSRC
+Tehran, Iran 
+License: Creative Commons 3.0 
+Notes to my future self:
+This modules fills hypointense lesions on 
+T1-weighted images and fills them with median
+intensity of normal appearing white matter
+
+Before initiating the functions, lesion maps should 
+be coregistered with T1 images. 
+
+"""
+
+from nipype.interfaces.base import BaseInterface, \
+        BaseInterfaceInputSpec, traits, File, TraitedSpec
+from nipype.utils.filemanip import split_filename
+import nibabel as nb
+import numpy as np 
+import os
+
+
+"""
+assuming T1 and lesion map are in
+the same space and co-registered
+this functions fills lesions with
+median intensity of the normal appearing
+white matter
+***
+T1 = t1 image
+wmp_map = white matter probability map
+resulting from priliminary segmentation
+without lesion filling
+lm = lesion map
+"""
+
+
+
+class FillLesionInputSpec(BaseInterfaceInputSpec):
+    T1 = File(exists = True, desc = 't1 volume which is \
+            which is already bias field corrected', mandatory = True)
+    lesion_map = File(exists = True, desc = 'lesion mask of \
+            flair or T2 scans', mandatory = True)
+    wmp_map = File(exists = True, desc = 'white matter probability map', 
+            mandatory = True)
+
+class FillLesionOutputSpec(TraitedSpec):
+    filled_t1 = File(exists = True, desc = 'lesion \
+            filled t1 volume')
+
+class LesionFill(BaseInterface):
+    input_spec = FillLesionInputSpec 
+    output_spec = FillLesionOutputSpec
+
+    def _run_interface(self, runtime):
+        t1_fname = self.inputs.T1
+        lm_fname = self.inputs.lesion_map
+        wmp_fname = self.inputs.wmp_map
+        #getting image and data via nibabel
+        t1_img = nb.load(t1_fname)
+        lm_img = nb.load(lesion_fname)
+        wm_img = nb.load(wmp_fname)
+        #converting to matrix 
+        t1_mx = t1_img.get_data()
+        lm_mx = lm_img.get_data()
+        wm_mx = wm_img.get_data()
+        #creating a binary wm map
+        wm_bn = wm_mx[:]
+        #binarising at a threshold of 0.4 
+        wm_bn[wm_mx > 0.4] = 1
+        wm_bn[wm_mx <= 0.4] = 0
+        #making sure wm lesion map is in fact binary
+        #(only 0 and 1)
+        lm_unique_arr = numpy.unique(lm_mx)
+        for i in lm_unique_arr:
+            if int(i) != 0 | int(i) !=1:
+                print "lesion map is not binary please recheck"
+                break
+        #nawm is the difference between wm probability
+        #and lesion map (remember lesion map and wm 
+        #probability map are in the same space
+        #first making a binary 
+        nawm_bn =  wm_bn - lm_mx
+        
+        #extracting median intenstiy of nawm
+        median_nawm = numpy.median(t1_mx[nawm_bn == 1])
+        #fill lesions 
+        lesion_fill = t1_mx[:]
+        lesion_fill[lm_mx == 1] = median_nawm
+        
+        #merging filled lesions with nawm
+        img = nibabel.Nifti1Image(lesion_fill, t1_img.get_affine(), t1_img.get_header())
+        _, base, _ = split_filename(t1_fname)
+        nb.save(img, base + '_filled.nii.gz')
+        return runtime
+
+        def _list_outputs(self):
+            outputs = self._outputs().get()
+            fname = self.inputs.t1_fname
+            _, base, _ = split_filename(fname)
+            outputs["filled_t1"] = os.path.abspath(base, '_filled.nii.gz')
+            return outputs
---
+++ test/test_lesion_filling.py
@@ -0,0 +1,5 @@
+from nose.tools import *
+from lesion.lesion_filling import *
+
+def test_lesion_filling():
+    None
