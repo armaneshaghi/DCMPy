3b2d2ad262a1dde790cc7a87a0a8a0ec17b8c2c1
Ubuntu <ubuntu@ip-172-31-37-230.ec2.internal>
2014-05-29 12:57:52 +0000

    added coveralls

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 tests:
 	nosetests
 dev_test:
-	notify 
+	./notify lesion
--- test/test_lesion_filling.py
+++ test/test_lesion_filling.py
@@ -3,3 +3,6 @@ from cogpy.lesion.lesion_filling import *
 
 def test_lesion_filling():
     None 
+
+def test_sag():
+    None
