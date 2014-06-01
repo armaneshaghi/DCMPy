b6a83316c81766f673539a16bf8a95d577328bb5
armaneshaghi <arman.eshaghi@gmail.com>
2014-06-01 13:45:22 +0000

    multiple slice support for visualise mri

--- visualise/mri.py
+++ visualise/mri.py
@@ -10,11 +10,13 @@ class image(object):
     def show(self, path, slice_number = 170):
         img = nb.load(path)
         img_data = img.get_data()
-        fig = plt.figure()
+        fig = plt.figure(figsize = (12, 15))
+        base_slice = slice_number - 30
         for i in range(6):
-            f =  fig.add_subplot(1, 2, i + 1)
-            plt.imshow(np.rot90(img_data[:, :, slice_number]),
-                    cmap = plt.cm.gray, 
+            f =  fig.add_subplot(3, 3, i + 1)
+            slice = base_slice + i * 10 
+            plt.imshow(np.rot90(img_data[:, :, slice]),
+                    cmap = plt.cm.gray,  aspect = 'auto', 
                     interpolation = 'nearest')
             plt.axis('off')
-            plt.title('Image')
+            plt.title('slice %d' %(slice))
