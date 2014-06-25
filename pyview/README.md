2c1742650e9add65dc08914a931f1a9d28cc85f9
armaneshaghi <arman.eshaghi@gmail.com>
2014-06-25 08:31:06 +0000

    updating pyview documentation and example usage

--- pyview/README.md
+++ pyview/README.md
@@ -8,3 +8,24 @@ makes use of advanced visualization and interactive widgets
 inside IPython notebook. Therefore, using it outside IPython
 notebook fails.
 
+---
+
+Example usage:
+
+```
+slices = np.zeros([4, 3])
+slices[0,0] = 128
+slices[1,0] = 118
+slices[2,0] = 100
+slices[3,0] = 90
+slices[0,1] = 128
+slices[1,1] = 100
+slices[2,1] = 40
+slices[3,1] = 128
+slices[0,2] = 100
+slices[1,2] = 90
+slices[2,2] = 128
+slices[3,2] = 100
+fscheck = mri.FS(subject_name = 'baseline_V_V1', slices = slices)
+fscheck.superimpose()
+```
--- pyview/mri.py
+++ pyview/mri.py
@@ -173,6 +173,24 @@ class FS(object):
     def superimpose(self):
         """
         main function to loop aroung all slices and plot images
+        Example usage:
+        Defining required slices (n by 3 numpy array):
+        > slices = np.zeros([4, 3])
+        > slices[0,0] = 128
+        > slices[1,0] = 118
+        > slices[2,0] = 100
+        > slices[3,0] = 90
+        > slices[0,1] = 128
+        > slices[1,1] = 100
+        > slices[2,1] = 40
+        > slices[3,1] = 128
+        > slices[0,2] = 100
+        > slices[1,2] = 90
+        > slices[2,2] = 128
+        > slices[3,2] = 100
+        
+        > fscheck = mri.FS(subject_name = 'baseline_V_V1', slices = slices)
+        > fscheck.superimpose()
         """
 
         slices = self.slices
