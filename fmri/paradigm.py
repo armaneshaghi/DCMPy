800002942a5bf380fae8eefd3e3ab57a4e8b77ae
armaneshaghi <arman.eshaghi@gmail.com>
2014-07-07 19:31:57 +0000

    fmri log text analyse

--- fmri/paradigm.py
+++ fmri/paradigm.py
@@ -39,7 +39,10 @@ class read(self,path, *args, **kwargs):
                return i
        return -1
 
-   def __experimentStart__(self, logContent):
+   def __scantStart__(self, logContent):
+       #giving the time at which the very first scan 
+       #has been acquired, TR = 2.0 
+       #first real scan = 7
        startLineIndex = self.__substringIndex__(logContent, 'Key\t33\tDOWN\tat')
        nextStartLineIndex = startLineIndex + 1 
        startLine = logContent[startLineIndex]
@@ -50,19 +53,41 @@ class read(self,path, *args, **kwargs):
        experiment_start_next = experiment_start_next.group(1)
        if experiment_start != experiment_start_next:
            raise Exception('ill-posed log file')
-       return double(experiment_start) 
-        
+       return double(experiment_start)
+   def __blockResult__(self, resContent):
+       for i in range(0, 30):
+           blockContent = resContent[0 + (i * 12), 12 + (i * 12)]
+           for j, s in enumerate(blockContent):
+               #first finding out what N back we are dealing with
+               n_backPat = re.compile("'(\d)-back'\s*.*")
+               n
+
+
+   def __blockAnalyse__(self, logContent):
+       #pattern of first stimulus of each block, group 1 = n-back load, 2 = time of onset
+       pat_firstStimOnset = re.compile('start of (\d-back) run number \d{1,2} at\
+               slice number \d{1,3} at time (\d{3,})')
+       #pattern of the last stimulus in log, group 1
+       pat_lastStimulus = re.compile('start of \d-back run number [0-9]{1,2} trial\
+               number 10 at slice number \d{1,3} at time (\d{3,})')
        
-       
-       
-   
+       for i, line in enumerate(logContent):
+           
+           
+           
+
+       startLineIndex = self.__substringIndex__(logContent)
+
+
+
    def 
        log = self.log
        res = self.res
        log_content = self.__content_read__(log)
        res_content = self.__content_read__(res)
        #setting start of the experiment
-       exOnset = self.__experimentStart__(log_content)
+       exOnset = self.__scanStart__(log_content)
+       
 
        
 
